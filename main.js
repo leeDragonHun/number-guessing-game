14002919ad19a3363c61352b70ea35daedfdc4ef
DragonHun Lee <9leeyh5@gmail.com>
2023-10-06 12:56:52 +0900

    third commit

--- main.js
+++ main.js
@@ -16,10 +16,14 @@ let resetButton = document.getElementById("reset-button");
 let chances = 5;
 let gameOver = false;
 let chanceArea = document.getElementById("chance-area");
+let history=[]
 
 playButton.addEventListener("click", play);
 resetButton.addEventListener("click", reset);
-
+userInput.addEventListener("focus",function(){
+  userInput.value = "";
+})
+~~
 function pickRandomNum(){
   computerNum = Math.floor(Math.random() * 100) + 1;
   console.log("정답", computerNum);
@@ -27,6 +31,15 @@ function pickRandomNum(){
 
 function play(){
   let userValue = userInput.value;
+
+  if(userValue<1 || userValue>100){
+    resultArea.textContent="1과 100사이 숫자를 입력해 주세요."
+    return;
+  }
+  if(history.includes(userValue)){
+    resultArea.textContent = "이미 입력한 숫자입니다.";
+    return;
+  }
 
   chances -- ;
   chanceArea.textContent=`남은기회:${chances}번`;
@@ -37,14 +50,18 @@ function play(){
     resultArea.textContent = "Down!!!";
   } else {
     resultArea.textContent = "정답!!!";
+    gameOver = true;
   }
 
+  history.push(userValue);
+  console.log(history);
+
   if(chances < 1){
-    gameOver = true
+    gameOver = true;
   }
 
   if(gameOver == true){
-    playButton.disabled = true
+    playButton.disabled = true;
   }
 }
 
